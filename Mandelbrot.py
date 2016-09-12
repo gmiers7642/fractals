489db11b33c35f85f596977fadfe25dd9f99776b
glenn <glenn@glenns-MacBook-Pro.local>
2016-09-12 09:20:37 -0500

    Added basic data frame extractor, and examples

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -5,10 +5,11 @@ Created on Wed Jun 15 20:08:18 2016
 @author: glenn
 """
 import numpy as np
+import pandas as pd
 
 class Mandelbrot(object):
     def __init__(self, params={'Xmin':-2.5, 'Xmax':1.5, 'Ymin':-2.0, 'Ymax':2.0,
-                     'maxIter':100, 'maxRad':3.0, 'gridSpc':0.05}):
+                     'maxIter':100, 'maxRad':100.0, 'gridSpc':0.05}):
         self.Xmin = params['Xmin']
         self.Xmax = params['Xmax']
         self.Ymin = params['Ymin']
@@ -40,8 +41,29 @@ class Mandelbrot(object):
                     iter += 1
                 self._image[yindex-1][xindex-1] = iter
 
-    def extract_df():
-        pass
+    def extract_df(self):
+        df = pd.DataFrame()
+        #For all grid points
+        #for y in self.yCoords:
+        for y in self.yCoords[45:51]:
+            yindex = int((self.Ymax - y) / self.gridSpc )
+            #for x in self.xCoords:
+            for x in self.xCoords[45:51]:
+                xindex = int((self.Xmax - x) / self.gridSpc )
+                z = complex(x,y)
+                c = z
+                iter = 0
+                s = []
+                while True:
+                    if abs(z) > self.maxRad or iter >= self.maxIter:
+                        break
+                    z = z**2 + c
+                    s.append(z)
+                    iter += 1
+                #print s
+                df['Z.' + str(yindex) + '_' + str(xindex)] = pd.Series(s)
+        return df
+                #self._image[yindex-1][xindex-1] = iter
 
     @property
     def image(self):
