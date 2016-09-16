c5221e7aa06eed1af4ca25040386c5787f38b384
glenn <glenn@glenns-mbp.gateway.pace.com>
2016-09-15 22:54:07 -0500

    Added test for plot_image

--- TestMandelbrot.py
+++ TestMandelbrot.py
@@ -3,8 +3,15 @@ import Mandelbrot as m
 import matplotlib.pyplot as plt
 import PlotUtils as p
 
+# Setup
 f1 = m.Mandelbrot()
 f1.setup()
-df_m = f1.extract_df()
 
-p.plot_iterations(df_m, 30, 34)
+# Extract df and plot iteration orbits
+#df_m = f1.extract_df()
+#p.plot_iterations(df_m, 30, 34)
+
+# Create and plot image of fractal
+f1.calc_image()
+arr = f1.image
+p.plot_image(arr)
